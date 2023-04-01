159614cb883eaec69d2ae4a093492b74b3254cf2
Mela Aliento <carmelaalientoZ@gmail.com>
2023-04-01 07:39:47 -0700

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,9 +4,8 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-const WorkboxPlugin = require("workbox-webpack-plugin");
+
 // TODO: Add CSS loaders and babel to webpack.
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => {
   return {
